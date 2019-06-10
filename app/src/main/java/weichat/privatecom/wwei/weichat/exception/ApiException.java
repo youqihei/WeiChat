c83d4e0f2e1b0d4eccf88f7c2c1f64c0c31785f4
youqihei <574419586@qq.com>
2019-06-10 17:11:58 +0800

    一些增加

---
+++ app/src/main/java/weichat/privatecom/wwei/weichat/exception/ApiException.java
@@ -0,0 +1,42 @@
+package weichat.privatecom.wwei.weichat.exception;
+
+import android.util.Log;
+
+/**
+ * Created by Administrator on 2019/6/6.
+ */
+
+public class ApiException extends Exception {
+    private int code;
+    private String displayMessage;
+
+    public ApiException(int code, String displayMessage) {
+        this.code = code;
+        this.displayMessage = displayMessage;
+    }
+
+    public ApiException(int code, String message, String displayMessage) {
+        super(message);
+        this.code = code;
+        this.displayMessage = displayMessage;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getDisplayMessage() {
+        return displayMessage;
+    }
+
+    public void setDisplayMessage(String displayMessage) {
+        this.displayMessage = displayMessage;
+    }
+
+
+}
+
